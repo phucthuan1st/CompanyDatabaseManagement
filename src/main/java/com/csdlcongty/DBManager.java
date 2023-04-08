ddf415a78411a937770f4267251b33d8d21f58ee
hongnhung-nguyen <83841301+hongnhung-nguyen@users.noreply.github.com>
2023-04-08 13:39:21 +0700

    last commit

--- src/main/java/com/csdlcongty/DBManager.java
+++ src/main/java/com/csdlcongty/DBManager.java
@@ -7,7 +7,10 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList; 
 /*
    Read note in Resources folder to learn more about ojdbc driver
 */
@@ -18,8 +21,10 @@ public class DBManager {
     protected Connection cnt;
     protected Statement st;
     protected CallableStatement  cst;
+    protected PreparedStatement  pst;
     protected String previousStatement;
-
+    
+    
     public DBManager(String username, String password) throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         cnt = DriverManager.getConnection(DBURL, username, password);
@@ -143,7 +148,7 @@ public class DBManager {
         return result;
     }
     
-       public int  createNewUser(String usernm, String pass) {
+    public int  createNewUser(String usernm, String pass) {
         String sql = "{call CREATE_USER(?, ?)}";
         int resultt= 0;
         try {
@@ -158,7 +163,7 @@ public class DBManager {
         
         return resultt;
     }
-       public int  createNewRole(String roleName) {
+    public int  createNewRole(String roleName) {
         String sql = "{call CREATE_ROLE(?)}";
         int hasResult=0;
         try {
@@ -168,6 +173,50 @@ public class DBManager {
             hasResult =1;
             //System.out.println("Record insert: ");
         
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return hasResult;
+    }
+    public int  createNewTable(String nametable, int numbercolumn, ArrayList<String> varname, ArrayList<String> vartype, ArrayList<String> varnumber, ArrayList<String> varvalue) {
+        String sql = "CREATE TABLE " +nametable +"( ";
+        
+        for (int i=0;i<numbercolumn; i++)
+        {
+        sql= sql+ varname.get(0);
+        varname.remove(0);
+        sql+= vartype.get(0);
+        if(vartype.get(0)== " VARCHAR " || vartype.get(0)== " NVARCHAR " || vartype.get(0)== " VARCHAR2 " || vartype.get(0)== " NVARCHAR2 ")
+        {   
+            sql+= "("+ varnumber.get(0)+")";
+            varnumber.remove(0);
+        }
+        
+        vartype.remove(0);
+        if(varvalue.get(0) != " PRIMARY KEY ")
+        {
+            sql+= varvalue.get(0);
+        }
+        else
+        {
+            sql+= " CONSTRAINT table_pk " + varvalue.get(0);
+        }
+        
+        varvalue.remove(0);
+        if(i<numbercolumn-1)
+        {
+            sql+= ", ";
+        }
+        }
+        sql+=")";
+        int hasResult=0;
+        try {
+            pst = cnt.prepareStatement(sql);
+            hasResult =pst.executeUpdate();
+            //System.out.println("Record insert: ");
+            hasResult=1;
+            
         } catch (SQLException ex) {
             Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
         }
