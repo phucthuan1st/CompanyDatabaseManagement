8c36a1b35f304e2cfed93ebcf606dbdff6f0d3c5
thach030103 <120116007+thach030103@users.noreply.github.com>
2023-06-15 23:41:16 +0700

    Update CS6

--- PLSQL/QLNhanVienDeAn_PhanHe2_Policies.sql
+++ PLSQL/QLNhanVienDeAn_PhanHe2_Policies.sql
@@ -347,3 +347,88 @@ CS#6: Những người dùng có VAITRO là “Trưởng đề án” cho biết
 − Có quyền như là một nhân viên thông thường (vai trò “Nhân viên”).
 − Được quyền thêm, xóa, cập nhật trên quan hệ ĐEAN.
 */
+CREATE OR REPLACE FUNCTION TRUONGDEAN_PERMISSION_CONSTRAINTS (
+  schema_name   IN VARCHAR2,
+  object_name   IN VARCHAR2
+)
+RETURN VARCHAR2
+IS
+  vaitro NVARCHAR2(20);
+BEGIN
+        -- Lấy vai trò của user hiện tại
+    SELECT VAITRO INTO vaitro FROM COMPANY_PUBLIC.NHANVIEN WHERE MANV = SYS_CONTEXT('USERENV', 'SESSION_USER');
+
+      -- Truy cập dòng liên quan đến nhân viên đó
+    IF object_name = 'NHANVIEN' THEN
+        IF vaitro = 'Trưởng đề án' THEN
+             RETURN 'MANV = SYS_CONTEXT(''USERENV'', ''SESSION_USER'')';
+        END IF;
+    
+    -- Truy cập dòng liên quan đến nhân viên đó
+    ELSIF object_name = 'PHANCONG' THEN
+        IF vaitro = 'Trưởng đề án' THEN
+             RETURN 'MANV = SYS_CONTEXT(''USERENV'', ''SESSION_USER'')';
+        END IF;
+    
+    -- tùy ý truy cập phòng ban và đề án
+    ELSIF object_name = 'PHONGBAN' or object_name = 'DEAN' THEN
+        IF vaitro = 'Trưởng đề án' THEN
+            RETURN '1=1'
+        END IF;
+    END IF;
+  
+  RETURN NULL;
+END;
+/
+-- *NOTE: đối với hàm chính sách, return NULL để có vô hiệu hóa điều kiện, để có thể gắn thêm các chính sách khác (các CS2 --> 6)
+
+BEGIN
+    -- Có thê xem dữ liệu của toàn bộ quan hệ PHONGBAN và DEAN như nhân viên.
+    DBMS_RLS.ADD_POLICY(
+        object_schema    => 'COMPANY_PUBLIC',
+        object_name      => 'PHONGBAN',
+        policy_name      => 'TRUONGDEAN_SELECT_PHONGBAN_POLICY',
+        function_schema  => 'COMPANY_PUBLIC',
+        policy_function  => 'TRUONGDEAN_PERMISSION_CONSTRAINTS',
+        statement_types  => 'SELECT'
+    );
+
+    DBMS_RLS.ADD_POLICY(
+        object_schema    => 'COMPANY_PUBLIC',
+        object_name      => 'DEAN',
+        policy_name      => 'TRUONGDEAN_SELECT_DEAN_POLICY',
+        function_schema  => 'COMPANY_PUBLIC',
+        policy_function  => 'TRUONGDEAN_PERMISSION_CONSTRAINTS',
+        statement_types  => 'SELECT'
+    );
+    
+    -- Có thể xem dữ liệu của toàn bô quan hệ NHANVIEN và PHANCONG liên quan đến nhân viên đó
+    DBMS_RLS.ADD_POLICY(
+        object_schema    => 'COMPANY_PUBLIC',
+        object_name      => 'NHANVIEN',
+        policy_name      => 'TRUONGDEAN_SELECT_NHANVIEN_POLICY',
+        function_schema  => 'COMPANY_PUBLIC',
+        policy_function  => 'TRUONGDEAN_PERMISSION_CONSTRAINTS',
+        statement_types  => 'SELECT'
+    );
+
+    DBMS_RLS.ADD_POLICY(
+        object_schema    => 'COMPANY_PUBLIC',
+        object_name      => 'PHANCONG',
+        policy_name      => 'TRUONGDEAN_SELECT_PHANCONG_POLICY',
+        function_schema  => 'COMPANY_PUBLIC',
+        policy_function  => 'TRUONGDEAN_PERMISSION_CONSTRAINTS',
+        statement_types  => 'SELECT'
+    );
+
+    -- Có thể thêm,xoá,cập nhật trên quan hệ DEAN
+    DBMS_RLS.ADD_POLICY(
+        object_schema    => 'COMPANY_PUBLIC',
+        object_name      => 'DEAN',
+        policy_name      => 'TRUONGDEAN_INSERT_DELETE_UPDATE_DEAN_POLICY',
+        function_schema  => 'COMPANY_PUBLIC',
+        policy_function  => 'TRUONGDEAN_PERMISSION_CONSTRAINTS',
+        statement_types  => 'INSERT,DELETE,UPDATE',
+    );
+END;
+/
